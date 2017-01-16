e756e6319ef53e997431e47b46d9038e431727e7
Sam Scislowicz <samantha.scislowicz@gmail.com>
2017-01-16 00:50:56 +0000

    style change

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
 def safe_print_list(my_list=[], x=0):
- if my_list is None or my_list == [] or x == 0:
-     print()
-     return (0)
+    if my_list is None or my_list == [] or x == 0:
+        print()
+        return (0)
     try:
         for i in range(x):
             print("{}".format(my_list[1]), end="")
