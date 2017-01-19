8d22a3047f887606ee8735bc1f6abd4258b63dd5
Sam Scislowicz <samantha.scislowicz@gmail.com>
2017-01-19 00:41:30 +0000

    pep8 update

--- 0x06-python-test_driven_development/3-say_my_name.py
+++ 0x06-python-test_driven_development/3-say_my_name.py
@@ -1,14 +1,17 @@
 #!/usr/bin/python3
 """
-This module defines the say_my_name function printing out the first_name and last_name
+This is the say_my_name module.
+This is to print out the first and last names that is typed in.
 """
+
+
 def say_my_name(first_name, last_name=""):
     """ Prints 2 strings as first and last name.
 
     first_name and last_name must be strings.
     """
     if type(first_name) != str:
-        raise TypeError ("first_name must be a string")
+        raise TypeError("first_name must be a string")
     elif type(last_name) != str:
-        raise TypeError ("last_name must be a string")
+        raise TypeError("last_name must be a string")
     print("My name is {} {}".format(first_name, last_name))
