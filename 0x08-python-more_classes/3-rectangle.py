e1021adaa0970441ca6eb4500500c44dbd22423f
Sam Scislowicz <samantha.scislowicz@gmail.com>
2017-01-25 02:34:12 +0000

    pep8 changes

--- 0x08-python-more_classes/3-rectangle.py
+++ 0x08-python-more_classes/3-rectangle.py
@@ -3,34 +3,41 @@ class Rectangle:
     def __init__(self, width=0, height=0):
         self.width = width
         self.height = height
+
         @property
         def width(self):
             return self.__width
+
         @width.setter
         def width(self, value):
             if type(width) != int:
                 raise TypeError('width must be an integer')
             if width < 0:
                 raise ValueError('width must be >= 0')
+
         @property
         def height(self):
             return self.__height
+
         @height.setter
         def height(self, value):
             if type(height) != int:
                 raise TypeError('height must be an integer')
             if height < 0:
                 raise ValueError('height must be >= 0')
+
     def area(self):
         return int(self.width) * int(self.height)
+
     def perimeter(self):
         if int(self.width) <= 0 or int(self.height) <= 0:
             return 0
         return (int(self.width) + int(self.height)) * 2
+
     def __str__(self):
         st = ""
         if (self.width == 0) or (self.height == 0):
-           return st
+            return st
         for h in range(self.height):
             for w in range(self.width):
                 st += "#"
