11b59d4a99f9dae75d20345054e6a9fc2bc5d9da
Sam Scislowicz <samantha.scislowicz@gmail.com>
2017-02-02 23:59:17 +0000

    cleaner

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -3,5 +3,5 @@ def read_file(filename=""):
     """
     Read a file with a with statement
     """
-    with open("my_file_0.txt", mode="r", encoding="utf-8") as my_file:
+    with open(filename, encoding="utf-8") as my_file:
         print(my_file.read())
