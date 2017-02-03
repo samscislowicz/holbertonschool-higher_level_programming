f6e8325fef04fffd560de263d793ecf216155c86
Sam Scislowicz <samantha.scislowicz@gmail.com>
2017-02-03 00:01:45 +0000

    initial commit

---
+++ 0x0B-python-input_output/1-number_of_lines.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+def number_of_lines(filename=""):
+    """
+    Number of lines in a file function
+    """
+    with open(filename, encoding="utf-8") as my_file:
+        l = 0
+        for line in my_file:
+            l += 1
+        return l
