aa6208ef1b9bb608527e27d9b7210c3091a7b073
Sam Scislowicz <samantha.scislowicz@gmail.com>
2017-02-23 03:21:32 +0000

    add file

---
+++ 0x0C-SQL_introduction/8-count_89.sql
@@ -0,0 +1,3 @@
+-- displays the number of records with id = 89 in the table first_table
+-- The database name will be passed as an argument of the mysql command
+SELECT count(id) FROM first_table WHERE id = '89';
