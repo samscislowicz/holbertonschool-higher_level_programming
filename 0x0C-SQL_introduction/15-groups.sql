008f63a49903b08e9421fec202693fac439186e9
Sam Scislowicz <samantha.scislowicz@gmail.com>
2017-02-23 03:54:23 +0000

    add file

---
+++ 0x0C-SQL_introduction/15-groups.sql
@@ -0,0 +1,3 @@
+-- script that lists the number of records with the same score in the table second_table
+-- The list should be sorted by the number of records (descending)
+SELECT score, count(score) AS number FROM second_table GROUP BY score ORDER BY number DESC;
