5c5aa5d9d50405ade777c7a21949a9770945f728
Sam Scislowicz <samantha.scislowicz@gmail.com>
2017-02-28 21:19:24 +0000

    added if not exists

--- 0x0D-SQL_more_queries/1-create_user.sql
+++ 0x0D-SQL_more_queries/1-create_user.sql
@@ -1,3 +1,3 @@
 -- Script to create new user
 -- User will have all privileges and if user already exists script will not fail
-CREATE USER 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1_pwd';
+CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1_pwd';
