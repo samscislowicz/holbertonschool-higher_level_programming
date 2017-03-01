59d531e0ac06ecae196580ed254e61d47c0ef52f
Sam Scislowicz <samantha.scislowicz@gmail.com>
2017-03-01 00:24:28 +0000

    added flush

--- 0x0D-SQL_more_queries/1-create_user.sql
+++ 0x0D-SQL_more_queries/1-create_user.sql
@@ -1,3 +1,4 @@
 -- Script to create new user
 -- User will have all privileges and if user already exists script will not fail
-CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1_pwd';
+GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1_pwd';
+FLUSH PRIVILEGES;
