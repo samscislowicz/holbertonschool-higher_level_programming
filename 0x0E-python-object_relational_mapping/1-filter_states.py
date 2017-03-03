18a002421156a83ff9e1fa60a8bfc13b3ae9527a
Sam Scislowicz <samantha.scislowicz@gmail.com>
2017-03-03 11:20:24 +0000

    Changed path to python3

--- 0x0E-python-object_relational_mapping/1-filter_states.py
+++ 0x0E-python-object_relational_mapping/1-filter_states.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python3
+#!/usr/bin/python3
 '''
 lists all states with a name starting with N from the database hbtn_0e_0_usa
 take 3 arguments: mysql username, mysql password and database name
