838bdf34fac0ca05b7ed322026b62499e59db2db
Sam Scislowicz <samantha.scislowicz@gmail.com>
2017-03-19 11:53:33 +0000

    changed format for pep8

--- 0x10-python-network_1/0-hbtn_status.py
+++ 0x10-python-network_1/0-hbtn_status.py
@@ -3,7 +3,8 @@ import urllib.request
 if __name__ == "__main__":
     with urllib.request.urlopen('http://intranet.hbtn.io/status') as response:
         html = response.read()
-print("Body response:")
-print("    - type: <class 'bytes'>")
-print("    - content: {}".format(html))
-print("    - utf8 content: OK")
+print("""Body response:"
+    - type: {}
+    - content: {}
+    - utf8 content: {:s}""".format(type(html), html,
+                                   html.decode(encoding='UTF-8')))
