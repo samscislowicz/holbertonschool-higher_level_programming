12fabb4e77af3f6305c7d8f01b2182fc02c74844
Sam Scislowicz <samantha.scislowicz@gmail.com>
2017-03-19 15:09:03 +0000

    added tabs

--- 0x10-python-network_1/0-hbtn_status.py
+++ 0x10-python-network_1/0-hbtn_status.py
@@ -3,8 +3,8 @@ import urllib.request
 if __name__ == "__main__":
     with urllib.request.urlopen('http://intranet.hbtn.io/status') as response:
         html = response.read()
-print("""Body response:"
-    - type: {}
-    - content: {}
-    - utf8 content: {:s}""".format(type(html), html,
+print("""Body response:
+\t- type: {}
+\t- content: {}
+\t- utf8 content: {:s}""".format(type(html), html,
                                    html.decode(encoding='UTF-8')))
