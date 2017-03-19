3efc809d121151c08253b9e642afa0eb626efa1a
Sam Scislowicz <samantha.scislowicz@gmail.com>
2017-03-19 15:25:30 +0000

    more pep8

--- 0x10-python-network_1/0-hbtn_status.py
+++ 0x10-python-network_1/0-hbtn_status.py
@@ -1,12 +1,13 @@
 #!/usr/bin/python3
 import urllib.request
-"""
-This module is a Python script that fetches https://intranet.hbtn.io/status
-"""
+if __name__ == "__main__":
+    """
+    This module is a Python script that fetches https://intranet.hbtn.io/status
+    """
 with urllib.request.urlopen('http://intranet.hbtn.io/status') as response:
     html = response.read()
     print("""Body response:
     - type: {}
     - content: {}
     - utf8 content: {}""".format(type(html), html,
-                                  html.decode(encoding='UTF-8')))
+                                 html.decode(encoding='UTF-8')))
