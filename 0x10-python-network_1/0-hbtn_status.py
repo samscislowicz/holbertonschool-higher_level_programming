5e49556a8287222a376d3e1464aa1f5e267dbf98
Sam Scislowicz <samantha.scislowicz@gmail.com>
2017-03-19 15:17:28 +0000

    added documentation

--- 0x10-python-network_1/0-hbtn_status.py
+++ 0x10-python-network_1/0-hbtn_status.py
@@ -1,10 +1,12 @@
 #!/usr/bin/python3
 import urllib.request
-if __name__ == "__main__":
-    with urllib.request.urlopen('http://intranet.hbtn.io/status') as response:
-        html = response.read()
-print("""Body response:
-\t- type: {}
-\t- content: {}
-\t- utf8 content: {:s}""".format(type(html), html,
-                                 html.decode(encoding='UTF-8')))
+"""
+This module is a Python script that fetches https://intranet.hbtn.io/status
+"""
+with urllib.request.urlopen('http://intranet.hbtn.io/status') as response:
+    html = response.read()
+    print("""Body response:
+    - type: {}
+    - content: {}
+    - utf8 content: {:}""".format(type(html), html,
+                                  html.decode(encoding='UTF-8')))
