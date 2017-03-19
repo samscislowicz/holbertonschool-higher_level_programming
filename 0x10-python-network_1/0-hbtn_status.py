4eaf6f35aadb8a7d2939d26d5445082a41d93eed
Sam Scislowicz <samantha.scislowicz@gmail.com>
2017-03-19 20:14:54 +0000

    indentation error

--- 0x10-python-network_1/0-hbtn_status.py
+++ 0x10-python-network_1/0-hbtn_status.py
@@ -5,7 +5,7 @@ This module is a Python script that fetches https://intranet.hbtn.io/status
 import urllib.request
 
 if __name__ == "__main__":
-        url = 'http://intranet.hbtn.io/status'
+    url = 'http://intranet.hbtn.io/status'
     with urllib.request.urlopen(url) as response:
         html = response.read()
         print("Body response:")
