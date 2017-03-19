32c44d7e8e1097b6ea58d05e2d558a0d5cdb39f3
Sam Scislowicz <samantha.scislowicz@gmail.com>
2017-03-19 20:13:39 +0000

    correction for checker

--- 0x10-python-network_1/0-hbtn_status.py
+++ 0x10-python-network_1/0-hbtn_status.py
@@ -5,7 +5,8 @@ This module is a Python script that fetches https://intranet.hbtn.io/status
 import urllib.request
 
 if __name__ == "__main__":
-    with urllib.request.urlopen('http://intranet.hbtn.io/status') as response:
+        url = 'http://intranet.hbtn.io/status'
+    with urllib.request.urlopen(url) as response:
         html = response.read()
         print("Body response:")
         print("\t- type: {}".format(type(html)))
