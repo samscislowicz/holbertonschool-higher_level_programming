b539cd6de60bcf55f0df78f56bdfc591ac67b24a
Sam Scislowicz <samantha.scislowicz@gmail.com>
2017-03-19 15:28:09 +0000

    looking for checks

--- 0x10-python-network_1/0-hbtn_status.py
+++ 0x10-python-network_1/0-hbtn_status.py
@@ -4,10 +4,10 @@ if __name__ == "__main__":
     """
     This module is a Python script that fetches https://intranet.hbtn.io/status
     """
-with urllib.request.urlopen('http://intranet.hbtn.io/status') as response:
-    html = response.read()
-    print("""Body response:
-    - type: {}
-    - content: {}
-    - utf8 content: {}""".format(type(html), html,
-                                 html.decode(encoding='UTF-8')))
+    with urllib.request.urlopen('http://intranet.hbtn.io/status') as response:
+        html = response.read()
+        print("""Body response:
+\t- type: {}
+\t- content: {}
+\t- utf8 content: {}""".format(type(html), html,
+                               html.decode(encoding='UTF-8')))
