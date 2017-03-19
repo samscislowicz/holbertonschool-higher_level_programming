6a7798d95f898c48805c25bcca747a81516acd40
Sam Scislowicz <samantha.scislowicz@gmail.com>
2017-03-19 15:29:59 +0000

    one more check comon

--- 0x10-python-network_1/0-hbtn_status.py
+++ 0x10-python-network_1/0-hbtn_status.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
+"""
+This module is a Python script that fetches https://intranet.hbtn.io/status
+"""
 import urllib.request
 if __name__ == "__main__":
-    """
-    This module is a Python script that fetches https://intranet.hbtn.io/status
-    """
     with urllib.request.urlopen('http://intranet.hbtn.io/status') as response:
         html = response.read()
         print("""Body response:
