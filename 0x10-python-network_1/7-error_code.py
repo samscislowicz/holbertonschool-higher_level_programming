a46d979955053be9db8652c9f2b0ff711065af72
Sam Scislowicz <samantha.scislowicz@gmail.com>
2017-03-19 19:30:10 +0000

    added doc

--- 0x10-python-network_1/7-error_code.py
+++ 0x10-python-network_1/7-error_code.py
@@ -1,4 +1,8 @@
 #!/usr/bin/python3
+"""
+a Python script that takes in a URL, sends a request to the URL
+and displays the body of the response.
+"""
 import requests
 import sys
 if __name__ == "__main__":
