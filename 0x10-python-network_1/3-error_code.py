f98b66484f4662969c601ef42860b01cd43599f5
Sam Scislowicz <samantha.scislowicz@gmail.com>
2017-03-19 13:40:52 +0000

    works

---
+++ 0x10-python-network_1/3-error_code.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+import urllib.parse
+import urllib.request
+import urllib.error
+import sys
+if __name__ == "__main__":
+
+    url = sys.argv[1]
+    req = urllib.request.Request(url)
+
+    try:
+        with urllib.request.urlopen(req) as response:
+            html = response.read()
+            print(html.decode(encoding='UTF-8'))
+    except urllib.error.HTTPError as e:
+        print('Error code: ', e.code)
