58db3413d7a5bb3a03c311d494f739604e03dd8a
Sam Scislowicz <samantha.scislowicz@gmail.com>
2017-03-19 15:43:35 +0000

    missing one check

--- 0x10-python-network_1/0-hbtn_status.py
+++ 0x10-python-network_1/0-hbtn_status.py
@@ -3,10 +3,11 @@
 This module is a Python script that fetches https://intranet.hbtn.io/status
 """
 import urllib.request
+
 if __name__ == "__main__":
     with urllib.request.urlopen('http://intranet.hbtn.io/status') as response:
         html = response.read()
         print("Body response:")
         print("\t- type: {}".format(type(html)))
         print("\t- content: {}".format(html))
-        print("\t- utf8 content: {}".format(html.decode(encoding='UTF-8')))
+        print("\t- utf8 content: {}".format(html.decode('UTF-8')))
