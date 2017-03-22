c53dcd2fcac7090c41cf1f921d5285e9c5a87429
Sam Scislowicz <samantha.scislowicz@gmail.com>
2017-03-22 12:45:02 +0000

    using let instead of const

---
+++ 0x11-javascript-warm_up/6-multi_languages_loop.js
@@ -0,0 +1,7 @@
+#!/usr/bin/node
+const lang = ['C is fun', 'Python is cool', 'Javascript is amazing'];
+const lLen = lang.length;
+let i = 0;
+for (i = 0; i < lLen; i++) {
+  console.log(lang[i]);
+}
