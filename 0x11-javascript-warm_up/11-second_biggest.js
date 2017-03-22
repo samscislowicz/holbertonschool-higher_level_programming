bde05810d2a715b0b9cfc3d038d3d4201c5f2f39
Sam Scislowicz <samantha.scislowicz@gmail.com>
2017-03-22 16:37:27 +0000

    removed =?

--- 0x11-javascript-warm_up/11-second_biggest.js
+++ 0x11-javascript-warm_up/11-second_biggest.js
@@ -1,5 +1,5 @@
 #!/usr/bin/node
-if (process.argv.length <= 4) {
+if (process.argv.length < 4) {
   console.log('0');
 } else {
   console.log(process.argv.slice(2, process.argv.length).sort() [process.argv.length - 4]);
