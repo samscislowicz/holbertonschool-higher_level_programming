46f7efb17eae621502e5ba1ba7103674350dd3fd
Sam Scislowicz <samantha.scislowicz@gmail.com>
2017-03-22 16:38:25 +0000

    formated

--- 0x11-javascript-warm_up/11-second_biggest.js
+++ 0x11-javascript-warm_up/11-second_biggest.js
@@ -2,5 +2,5 @@
 if (process.argv.length < 4) {
   console.log('0');
 } else {
-  console.log(process.argv.slice(2, process.argv.length).sort() [process.argv.length - 4]);
+  console.log(process.argv.slice(2, process.argv.length).sort()[process.argv.length - 4]);
 }
