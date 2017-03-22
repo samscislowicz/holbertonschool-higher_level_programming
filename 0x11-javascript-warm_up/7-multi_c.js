62659cf15bf63920a8b70e8dc7b3cd828de1e372
Sam Scislowicz <samantha.scislowicz@gmail.com>
2017-03-22 18:09:16 +0000

    fixed

--- 0x11-javascript-warm_up/7-multi_c.js
+++ 0x11-javascript-warm_up/7-multi_c.js
@@ -1,9 +1,9 @@
 #!/usr/bin/node
 let x = parseInt(process.argv[2]);
-let c = 'C is fun\n'.repeat(x);
-while (isNaN(x) === true) {
+let c = 'C is fun';
+if (isNaN(x) === true) {
   console.log('Missing number of occurrences');
 }
-if (isNaN(x) === false) {
-  console.log(c.replace(/\n$/, ''));
+for (let i = 0; i < x; i++) {
+  console.log(c);
 }
