84a2f12b0c32415ad32dc83f19cd64b43a0db6d9
Sam Scislowicz <samantha.scislowicz@gmail.com>
2017-03-22 23:10:25 +0000

    finished

---
+++ 0x12-javascript-web_scraping/8-writeme.js
@@ -0,0 +1,9 @@
+#!/usr/bin/node
+var fs = require('fs');
+let file = process.argv[2];
+let string = process.argv[3];
+fs.writeFile(file, string, 'utf8', (err) => {
+  if (err) {
+    return console.log(err);
+  }
+});
