51e753751fb2fc9b39e650c72f96c441fa92db86
Sam Scislowicz <samantha.scislowicz@gmail.com>
2017-03-22 23:08:29 +0000

    fixed error handeling

--- 0x12-javascript-web_scraping/7-readme.js
+++ 0x12-javascript-web_scraping/7-readme.js
@@ -2,6 +2,8 @@
 var fs = require('fs');
 let path = process.argv[2];
 fs.readFile(path, 'utf8', (err, data) => {
-  if (err) throw err;
+  if (err) {
+    return console.log(err);
+  }
   console.log(data);
 });
