ceffd6c600695955a03ba6fd4916e0afac3fd698
Sam Scislowicz <samantha.scislowicz@gmail.com>
2017-03-22 22:14:35 +0000

    finished

---
+++ 0x12-javascript-web_scraping/5-square.js
@@ -0,0 +1,30 @@
+#!/usr/bin/node
+function Rectangle (w, h) {
+  if (w <= 0) {
+    var w = {};
+  }
+  if (h <= 0) {
+    var h = {};
+  } else {
+    this.width = w;
+    this.height = h;
+  }
+  this.print = function () {
+    for (let i = 0; i < this.height; i++) {
+      console.log(('X').repeat(this.width));
+    }
+  };
+  this.rotate = function () {
+    this.height = w;
+    this.width = h;
+  };
+  this.double = function () {
+    this.height = h += h;
+    this.width = w += w;
+  };
+}
+function Square (size) {
+  Rectangle.call(this, size, size);
+}
+exports.Square = Square;
+exports.Rectangle = Rectangle;
