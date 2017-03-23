e087a727e81b3798a59057b21e4c5e82cf8d5ec6
Sam Scislowicz <samantha.scislowicz@gmail.com>
2017-03-23 07:40:22 +0000

    add file

---
+++ 0x13-javascript-web_jquery/16-script.js
@@ -0,0 +1,4 @@
+//updates the text color of the HTML tag HEADER to red (#FF0000) when the user clicks on the tag DIV#red_header
+$('div#red_header').click(function () {
+  $('header'.css('color', '#FF0000');
+});
