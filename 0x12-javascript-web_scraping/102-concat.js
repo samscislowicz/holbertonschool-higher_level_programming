8d64d5b1fb0d6e280b9a289879d613985027473a
Sam Scislowicz <samantha.scislowicz@gmail.com>
2017-03-23 06:20:31 +0000

    no var

--- 0x12-javascript-web_scraping/102-concat.js
+++ 0x12-javascript-web_scraping/102-concat.js
@@ -1,5 +1,5 @@
 #!/usr/bin/node
-var concat = require('concat-files');
+let concat = require('concat-files');
 let firstfile = process.argv[2];
 let secondfile = process.argv[3];
 let destination = process.argv[4];
